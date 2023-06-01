4707091debadf3ca6edc94838c1214452240eee4
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-06-01 15:46:57 -0500

    got homepage to work

--- src/App.js
+++ src/App.js
@@ -7,27 +7,34 @@ import { useEffect, useState, useRef } from 'react';
 function App() {
   const [section, setSection] = useState("");
   // const ref = useRef();
-
-  // useEffect(() => {
-  //   // scroll to the top of the element
-  //   ref.current.scrollTop({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  // }, []);
+  
+  useEffect(() => {
+    const paths = ["/Contact", "/AboutMe", "/Portfolio", "/Resume"]
+    const url = window.location.href.replace(/^http(s?):\/\//i, "");
+    const pageArr = url.split(window.location.host)
+    console.log(pageArr)
+    const page = pageArr.splice(1, 1)
+    console.log(page)
+    if (!paths.includes(page)) {
+      setSection('/AboutMe')
+    }
+    setSection(page)
+  }, [window.location.href])
+  
+  console.log(section)
   return (
     <div className='vh-100 scroll'>
       <Header />
-      {window.location.href.includes("/Contact") && (
+      {section[0] === "/Contact" && (
         <Contact />
       )}
-      {window.location.href.includes("/AboutMe") && (
+      {section[0] === "/AboutMe" && (
         <AboutMe />
       )}
-      {window.location.href.includes("/Portfolio") && (
+      {section[0] === "/Portfolio" && (
         <Portfolio />
       )}
-      {window.location.href.includes("/Resume") && (
+      {section[0] === "/Resume" && (
         <Resume />
       )}
       <Footer className="mt-2" />
